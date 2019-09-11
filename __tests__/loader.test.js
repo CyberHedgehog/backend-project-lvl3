16faff3f5a56aa762dc96b538f572c0e652b9dd3
CyberHedgehog <reg.dobr@live.ru>
2019-09-11 12:20:55 +0000

    First step | test coverage setting

--- __tests__/loader.test.js
+++ __tests__/loader.test.js
@@ -11,32 +11,6 @@ const host = 'http://localhost';
 axios.defaults.host = host;
 axios.defaults.adapter = httpAdapter;
 
-// const vars = {};
-// beforeAll(async () => {
-//   vars.tmpDirectory = await fs.mkdtemp(path.join(tmpdir(), 'pl-'));
-//   vars.testFilePath = '__tests__/__fixtures__/file.html';
-//   vars.testFileContent = await fs.readFile(vars.testFilePath, 'utf-8');
-//   nock(host)
-//     .get('/page')
-//     .reply(200, vars.testFileContent)
-//     .get('/')
-//     .reply(200, vars.testFileContent);
-// });
-
-// test('Work', async (done) => {
-//   await loader(`${host}/page`, vars.tmpDirectory);
-//   const newFileContent = await fs.readFile(path.join(vars.tmpDirectory, 'localhost-page.html'), 'utf-8');
-//   expect(newFileContent).toBe(vars.testFileContent);
-//   done();
-// });
-
-// test('Work on main page', async (done) => {
-//   await loader(`${host}`, vars.tmpDirectory);
-//   const newFileContent = await fs.readFile(path.join(vars.tmpDirectory, 'localhost.html'), 'utf-8');
-//   expect(newFileContent).toBe(vars.testFileContent);
-//   done();
-// });
-
 test.each([
   ['/', 'localhost.html'],
   ['/page', 'localhost-page.html'],
