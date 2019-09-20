e979e5cdacaeda71c3e0ca6c9d42bbd5cb63b5ef
CyberHedgehog <reg.dobr@live.ru>
2019-09-20 07:42:41 +0000

    Step 2 done

--- __tests__/loader.test.js
+++ __tests__/loader.test.js
@@ -11,30 +11,11 @@ const host = 'http://localhost';
 axios.defaults.host = host;
 axios.defaults.adapter = httpAdapter;
 
-// test.each([
-//   ['/', 'localhost.html'],
-//   ['/page', 'localhost-page.html'],
-// ])(
-//   'Should work',
-//   async (link, fileName, done) => {
-//     const tmpDirectory = await fs.mkdtemp(path.join(tmpdir(), 'pl-'));
-//     const testFilePath = '__tests__/__fixtures__/page.html';
-//     const testFileContent = await fs.readFile(testFilePath, 'utf-8');
-//     nock(host)
-//       .get(link)
-//       .reply(200, testFileContent);
-//     await loader(`${host}${link}`, tmpDirectory);
-//     const newFileContent = await fs.readFile(path.join(tmpDirectory, fileName), 'utf-8');
-//     expect(newFileContent).toBe(testFileContent);
-//     done();
-//   },
-// );
 const fixturesDir = '__tests__/__fixtures__/';
+const testFilePath = path.join(fixturesDir, 'page.html');
+const testFilesDir = path.join(fixturesDir, 'files');
 
-test('Should work with links', async (done) => {
-  const tmpDirectory = await fs.mkdtemp(path.join(tmpdir(), 'pl-'));
-  const testFilePath = path.join(fixturesDir, 'page.html');
-  const testFilesDir = path.join(fixturesDir, 'files');
+beforeEach(async () => {
   const testFileContent = await fs.readFile(testFilePath, 'utf-8');
   nock(host)
     .get('/')
@@ -45,11 +26,12 @@ test('Should work with links', async (done) => {
     .reply(200, await fs.readFile(`${testFilesDir}/style.css`))
     .get('/files/script.scr')
     .reply(200, await fs.readFile(`${testFilesDir}/script.scr`));
+});
 
+test('Donwload files', async (done) => {
+  const tmpDirectory = await fs.mkdtemp(path.join(tmpdir(), 'pl-'));
   await loader(host, tmpDirectory);
   const tmpFilesList = await fs.readdir(path.join(tmpDirectory, 'localhost_files'));
-  const filesList = await fs.readdir(testFilesDir);
-  console.log(filesList);
   expect(tmpFilesList).toContain('files-logo.png');
   done();
 });
